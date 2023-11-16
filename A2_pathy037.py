55dee5da6292e91d4e5fd91aa942412209e40699
HarshPatel093 <harshpatel.adl@gmail.com>
2023-11-16 20:12:49 +1030

    I've included most of the class in both the files

--- A2_pathy037.py
+++ A2_pathy037.py
@@ -6,3 +6,16 @@ StudentID: 110403785
 EmailID: pathy037@mymail.unisa.edu.au
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
+
+rom abc import ABC, abstractmethod
+
+class Alchemist:
+    def _init_(self, attack, strength, defense, magic, ranged, necromancy):
+        self.__attack = attack
+        self.__strength = strength
+        self.__defense = defense
+        self.__magic = magic
+        self.__ranged = ranged
+        self.__necromancy = necromancy
+        self.__laboratory = Laboratory(self)
+        self.__recipes = {}
--- test_A2_pathy037.py
+++ test_A2_pathy037.py
@@ -1,8 +1,21 @@
 '''
 File: A2_pathy037.py
-Description: This file has the main code of the assignment.
+Description: This file has the testing code of the assignment.
 Author: Harsh Manishkumar Patel
 StudentID: 110403785
 EmailID: pathy037@mymail.unisa.edu.au
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
+
+rom abc import ABC, abstractmethod
+
+class Alchemist:
+    def _init_(self, attack, strength, defense, magic, ranged, necromancy):
+        self.__attack = attack
+        self.__strength = strength
+        self.__defense = defense
+        self.__magic = magic
+        self.__ranged = ranged
+        self.__necromancy = necromancy
+        self.__laboratory = Laboratory(self)
+        self.__recipes = {}
